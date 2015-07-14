f3140236b8837d5c7f1d36ec91e9485d7f014e1b
nichoth <nichoth@gmail.com>
2015-07-14 16:49:20 -0700

    render content with hyperstream

--- example/sspa/bin/render.js
+++ example/sspa/bin/render.js
@@ -1,9 +1,26 @@
 var path = require('path');
-var indexTmpl = require('../src/view/index.js');
 var fs = require('fs');
 var writeStreamP = require('writestreamp');
+var walk = require('fs-walk');
+var indexTmpl = require('../src/view/index.js');
 
-// render winston page
-indexTmpl(require('../src/view/winston'))
-  .pipe(writeStreamP(path.resolve(__dirname, '../dist/winston/index.html')))
-;
+var p = '/Users/nick/code/sspa/example/sspa/src/view/';
+var dist = '/Users/nick/code/sspa/example/sspa/dist/';
+
+// render everything -- pages and api endpoints
+walk.walk(
+  path.join(__dirname, '../src/view'),
+  function(basedir, filename, stat, next) {
+    if (stat.isDirectory()) {
+      var fullPath = path.join(basedir, filename);
+      indexTmpl(fs.createReadStream(fullPath+'/index.html'))
+        .pipe(writeStreamP(path.join(dist, filename, 'index.html')))
+      ;
+      // just copy file to api endpoint
+      // fs.createReadStream(fullPath+'/index.html')
+      //   .pipe(writeStreamP(path.join(dist, 'api', filename, 'index.html')))
+      // ;
+      next();
+    }
+  }
+);
